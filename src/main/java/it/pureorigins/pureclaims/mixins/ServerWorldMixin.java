3b38b1efab7464b0382a1656508d8f1a6ea8652e
Raymark97 <marcoliga97@gmai.com>
2021-11-24 15:44:25 +0100

    rewrote explosion handling

--- src/main/java/it/pureorigins/pureclaims/mixins/ServerWorldMixin.java
+++ src/main/java/it/pureorigins/pureclaims/mixins/ServerWorldMixin.java
@@ -2,11 +2,9 @@ package it.pureorigins.pureclaims.mixins;
 
 import it.pureorigins.pureclaims.PureClaims;
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.TntEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.mob.MobEntity;
-import net.minecraft.entity.projectile.FireballEntity;
-import net.minecraft.entity.projectile.WitherSkullEntity;
 import net.minecraft.network.packet.s2c.play.ExplosionS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
@@ -20,6 +18,7 @@ public class ServerWorldMixin {
 
   /**
    * @author Raymark
+   * @reason Hard to inject
    */
   @Overwrite
   public Explosion createExplosion(Entity entity, DamageSource damageSource, ExplosionBehavior behavior, double x, double y, double z, float power, boolean createFire, Explosion.DestructionType destructionType) {
@@ -27,26 +26,32 @@ public class ServerWorldMixin {
     Explosion explosion = new Explosion(instance, entity, damageSource, behavior, x, y, z, power, createFire, destructionType);
     explosion.collectBlocksAndDamageEntities();
 
+    LivingEntity causingEntity = explosion.getCausingEntity();
+    ServerPlayerEntity causingPlayer = null;
 
-    if (entity instanceof MobEntity mob) { //Creeper explosions
+    if (causingEntity instanceof ServerPlayerEntity player) causingPlayer = player;
+    else if (causingEntity instanceof MobEntity mob)
+      if (mob.getTarget() instanceof ServerPlayerEntity player) causingPlayer = player;
+
+    if (causingPlayer != null) {
+      ServerPlayerEntity finalCausingPlayer = causingPlayer;
+      explosion.getAffectedBlocks().removeIf(block -> !PureClaims.INSTANCE.checkEditPermissions(finalCausingPlayer, block));
+    }
+
+    /*if (entity instanceof MobEntity mob) { //Creeper explosions
       if (mob.getTarget() instanceof ServerPlayerEntity player)
         explosion.getAffectedBlocks().removeIf(block -> !PureClaims.INSTANCE.checkEditPermissions(player, block));
-    } else if (entity instanceof FireballEntity fireball) { //Fireball explosions
-      if (fireball.getOwner() instanceof ServerPlayerEntity player) {
+    } else if (entity instanceof ExplosiveProjectileEntity projectile) { //Fireball and wither explosions
+      if (projectile.getOwner() instanceof ServerPlayerEntity player) {
         explosion.getAffectedBlocks().removeIf(block -> !PureClaims.INSTANCE.checkEditPermissions(player, block));
-      } else if (fireball.getOwner() instanceof MobEntity thrower) {
+      } else if (projectile.getOwner() instanceof MobEntity thrower) {
         if (thrower.getTarget() instanceof ServerPlayerEntity target)
           explosion.getAffectedBlocks().removeIf(block -> !PureClaims.INSTANCE.checkEditPermissions(target, block));
       }
     } else if (entity instanceof TntEntity tnt) { //TNT explosions
       if (tnt.getCausingEntity() instanceof ServerPlayerEntity player)
         explosion.getAffectedBlocks().removeIf(block -> !PureClaims.INSTANCE.checkEditPermissions(player, block));
-    } else if (entity instanceof WitherSkullEntity skull) { //Wither Skull explosions
-      if (skull.getOwner() instanceof MobEntity thrower) {
-        if (thrower.getTarget() instanceof ServerPlayerEntity target)
-          explosion.getAffectedBlocks().removeIf(block -> !PureClaims.INSTANCE.checkEditPermissions(target, block));
-      }
-    }
+    }*/
 
     explosion.affectWorld(false);
     if (destructionType == Explosion.DestructionType.NONE) {
